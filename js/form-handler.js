1842d5ee1e608d66aaa5317795968a115c5bf65f
yaya <yaya@WatanabeNaokis-MacBook-Pro.local>
2018-07-19 10:29:25 +0900

    Deploy calculator

--- js/form-handler.js
+++ js/form-handler.js
@@ -1,17 +1,11 @@
 function myCheck() {
-    var flag = false; // 選択されているか否かを判定する変数
-  
-    for (var i = 0; i < document.form1.fruits.length; i++) {
-  
-      // i番目のチェックボックスがチェックされているかを判定
-      if (document.form1.fruits[i].checked) {
-        flag = true;    
-        alert(document.form1.fruits[i].value + "が選択されました。");
+    var fruits = document.fruitform.fruits
+    var sum = 0;
+    for (var i = 0; i < fruits.length; i++) {
+      if (fruits[i].checked) {
+        console.log(typeof(sum));
+        sum += parseInt(fruits[i].value);
       }
     }
-  
-    // 何も選択されていない場合の処理   
-    if (!flag) {
-      alert("項目が選択されていません。");
-    }
+    document.getElementById("fruit-sum").innerHTML = sum;
   }
